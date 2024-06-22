1cbcb22bb444c42de89662631b259fda389b2066
sonwonjae <sonwj091552@gmail.com>
2024-06-22 15:27:01 +0900

    chore: version update

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,4 +1,10 @@
-function Test() {
-    return <div>test - sonwonjae</div>
+import type { PropsWithChildren } from "react"
+
+interface TestProps extends PropsWithChildren {
+    test: 'a' | 'b'
+}
+
+function Test({ test, children }: TestProps) {
+    return <div>test-[{test}] - sonwonjae{children}</div>
 }
 export default Test
